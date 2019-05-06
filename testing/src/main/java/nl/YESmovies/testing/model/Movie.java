f3fa44694b0bf1b5a8002b6f6ddd4dda725b2b2a
unknown <joleinvandersluis@gmail.com>
2019-05-06 14:42:10 +0200

    cleaned up version with just getters and setters of fields

--- testing/src/main/java/nl/YESmovies/testing/model/Movie.java
+++ testing/src/main/java/nl/YESmovies/testing/model/Movie.java
@@ -10,55 +10,15 @@ import java.util.Map;
 
 @Entity
 public class Movie {
-    public static ArrayList<Movie> movieObjectList = new ArrayList<>();
-    public static ArrayList<String> movieList = new ArrayList<>();
-    //private ArrayList<Float> ratingList = new ArrayList<Float>() ;                 //all rating for one movie. moved to Movie Class
-    private HashMap<String,Float> ratingList = new HashMap<String, Float>();
-
-    public static int movieCounter = 0;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     private String title;
     private short releaseYear;
     private float yesRating;
     private int nrRatings;
     private float imdbRating;
-
-    public void addYesRating(String userName, float rating) {
-        ratingList.put(userName, rating);
-        double total = 0;
-        for(Map.Entry<String, Float> rated : ratingList.entrySet()){
-            total += rated.getValue();
-        }
-        this.nrRatings = ratingList.size();
-
-        this.yesRating = (float)Math.round((total / nrRatings)*10)/10;
-    }
-
-    public HashMap<String,Float> getRatingList(){
-        return ratingList;
-    }
-
-    public float getYesRating() {
-        return this.yesRating;
-    }
-    
-    public Movie(String title, short releaseYear, float imdbRating) {
-        this.title = title;
-        this.releaseYear = releaseYear;
-        this.id = ++movieCounter;
-        this.imdbRating = imdbRating;
-        movieList.add(title);
-    }
-
-    private String[] genresString = {"Action", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Documentary",
-            "Drama", "Family", "Fantasy", "Film Noir", "History", "Horror", "Music", "Musical", "Mystery", "Romance",
-            "Sci-Fi", "Short", "Sport", "Superhero", "Thriller", "War", "Western"};
-
-    private int[] genreInt = new int[genresString.length];
 
     public long getId() {
         return id;
@@ -80,26 +40,20 @@ public class Movie {
         this.releaseYear = releaseYear;
     }
 
-    public int[] getGenreInt() {
-        return genreInt;
+    public float getYesRating() {
+        return yesRating;
     }
 
-    public void addGenre(int index) {
-        this.genreInt[index] = 1;
+    public void setYesRating(float yesRating) {
+        this.yesRating = yesRating;
     }
 
-    public void removeGenre(int index) {
-        this.genreInt[index] = 0;
+    public int getNrRatings() {
+        return nrRatings;
     }
 
-    public String getGenreString(){
-        String movieGenre = "";
-        for (int i = 0; i < genresString.length; i++){
-            if(genreInt[i] == 1){
-                movieGenre += genresString[i]+" ";
-            }
-        }
-        return movieGenre;
+    public void setNrRatings(int nrRatings) {
+        this.nrRatings = nrRatings;
     }
 
     public float getImdbRating() {
@@ -108,15 +62,5 @@ public class Movie {
 
     public void setImdbRating(float imdbRating) {
         this.imdbRating = imdbRating;
-    }
-
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", releaseYear=" + releaseYear +
-                ", genres=" + this.getGenreString() +
-                '}';
     }
 }
--- testing/src/main/java/nl/YESmovies/testing/model/YesProfile.java
+++ testing/src/main/java/nl/YESmovies/testing/model/YesProfile.java
@@ -9,47 +9,12 @@ import java.util.HashMap;
 
 @Entity
 public class YesProfile {
-    public static ArrayList<YesProfile> profileObjectList = new ArrayList<>();
-    public static ArrayList<String> profileList = new ArrayList<>();
-    private HashMap<String,Float> myRatingsList = new HashMap<String,Float>();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     private String userName;
     private ArrayList<String> watchedMovies;
-    private String[] preferredGenresString = {"Action", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Documentary",
-            "Drama", "Family", "Fantasy", "Film Noir", "History", "Horror", "Music", "Musical", "Mystery", "Romance",
-            "Sci-Fi", "Short", "Sport", "Superhero", "Thriller", "War", "Western"};
-    private int[] preferredGenresInt = new int[preferredGenresString.length];
-
-
-    public void setMyRating(String movieTitle, float ratingGiven) {
-        myRatingsList.put(movieTitle,ratingGiven);
-    }
-
-    public float getMyRating(String movieTitle){
-        if(myRatingsList.get(movieTitle)==null){
-            if (Movie.movieList.contains(movieTitle)){
-                System.out.println("You have not yet Rated this movie");
-                return -1;
-                // add option to immediately rate the movie
-            } else {
-                System.out.println("The movie title was not recognised ");
-                return -2;
-                //make sure no invalid names can be passed
-                //did you mean ....? option
-                //or just show a selection of movies you rated
-            }
-        }
-        System.out.println("you gave " + movieTitle + " a rating of " + myRatingsList.get(movieTitle));
-        return myRatingsList.get(movieTitle);
-    }
-
-    public HashMap<String, Float> getMyRatingsList() {
-        return myRatingsList;
-    }
 
     public long getId() {
         return id;
@@ -69,35 +34,5 @@ public class YesProfile {
 
     public void setWatchedMovies(ArrayList<String> watchedMovies) {
         this.watchedMovies = watchedMovies;
-    }
-
-    //Traditional getter
-    public int[] getPreferredGenresArray() {
-        return this.preferredGenresInt;
-    }
-
-    //Getter that displays the preferred genres in text form
-    public String getPreferredGenresText() {
-        String output = "";
-        for (int i = 0; i < preferredGenresInt.length; i++) {
-            if (preferredGenresInt[i] == 1) {
-               output += preferredGenresString[i]+" ";
-            }
-        }
-        return output;
-    }
-
-    public void addPreferredGenres(int index) {
-        this.preferredGenresInt[index] = 1;
-    }
-
-    public void removePreferredGenres(int index) {
-        this.preferredGenresInt[index] = 0;
-    }
-
-    public YesProfile(String username) {
-        this.userName = username;
-        profileList.add(username); //Add username to the ArrayList with all usernames
-        profileObjectList.add(this);
     }
 }
