252933451874e69adf3c494cc48954b94bf4fae9
elsjen <elsje_nieboer_1@hotmail.com>
2019-05-06 15:14:34 +0200

    Fix api name to genres

--- testing/src/main/java/nl/YESmovies/testing/api/GenreController.java
+++ testing/src/main/java/nl/YESmovies/testing/api/GenreController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/movies")
+@RequestMapping("api/genres")
 public class GenreController {
 
     private GenreService genreService;
