930353bb4546383f2dc13a7b68780e5fe01b92c7
Suzanne <suzanne.veenhouwer@hotmail.com>
2019-05-13 15:16:52 +0200

    Add check valid releaseyear

--- testing/src/main/java/nl/YESmovies/testing/api/MovieController.java
+++ testing/src/main/java/nl/YESmovies/testing/api/MovieController.java
@@ -6,10 +6,10 @@ import nl.YESmovies.testing.model.Rating;
 import nl.YESmovies.testing.persistence.GenreRepository;
 import nl.YESmovies.testing.service.GenreService;
 import nl.YESmovies.testing.service.MovieService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @RestController
@@ -32,7 +32,12 @@ public class MovieController {
 
     @PostMapping
     public Movie create(@RequestBody Movie movie){
-        return this.movieService.save(movie);
+        LocalDate now = LocalDate.now();
+        if (movie.getReleaseYear() > 1888 && movie.getReleaseYear() < now.getYear()) {
+            return this.movieService.save(movie);
+        } else {
+            return null; //fix this later
+        }
     }
 
     @GetMapping("{id}")
